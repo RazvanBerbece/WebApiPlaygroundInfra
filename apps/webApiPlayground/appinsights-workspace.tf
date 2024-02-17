b05db7061b4b6511519c579e8185a2b2e7f03fe8
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2024-02-17 21:22:49 +0000

    fixes log analytics sku input (2)

--- apps/webApiPlayground/appinsights-workspace.tf
+++ apps/webApiPlayground/appinsights-workspace.tf
@@ -3,5 +3,5 @@ resource "azurerm_log_analytics_workspace" "webapi-appinsights-workspace" {
   location            = azurerm_resource_group.webapi-resource-group.location
   resource_group_name = azurerm_resource_group.webapi-resource-group.name
   sku                 = "PerGB2018"
-  retention_in_days   = 7
+  retention_in_days   = 30
 }
