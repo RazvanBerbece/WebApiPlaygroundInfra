1b987094a7b25a3ec9e83d3894eda0bd9a41e333
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2024-02-19 22:14:13 +0000

    updates default image for webap capp

--- apps/webApiPlayground/container-apps.tf
+++ apps/webApiPlayground/container-apps.tf
@@ -10,7 +10,7 @@ resource "azurerm_container_app" "webapi-capp" {
 
     container {
       name   = "webapiplayground"
-      image  = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
+      image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
       cpu    = 0.25
       memory = "0.5Gi"
     }
