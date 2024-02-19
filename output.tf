8ff37530bcd3496f9ff525fa5c2609798aeebd49
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2024-02-19 03:58:46 +0000

    formats

--- output.tf
+++ output.tf
@@ -42,7 +42,7 @@ output "acr_login_server" {
 
 # Web API Playground 
 output "webApi_resource_group_id" {
-  value = module.webApiPlayground.webApi_resource_group_id
+  value     = module.webApiPlayground.webApi_resource_group_id
   sensitive = true
 }
 
