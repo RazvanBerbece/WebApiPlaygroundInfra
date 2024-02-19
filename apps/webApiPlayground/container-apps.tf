7e48faa41d2a9bf6bfd42c546bf04fbe2e5a2b47
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2024-02-19 19:34:57 +0000

    removes networking rule for capp

--- apps/webApiPlayground/container-apps.tf
+++ apps/webApiPlayground/container-apps.tf
@@ -3,17 +3,7 @@ resource "azurerm_container_app" "webapi-capp" {
   container_app_environment_id = azurerm_container_app_environment.webapi-cae.id
   resource_group_name          = azurerm_resource_group.webapi-resource-group.name
   revision_mode                = "Single"
-
-  # By default, allow all public access and send 100% of traffic to the instance
-#   ingress {
-#     target_port      = 8080
-#     external_enabled = true
-
-#     traffic_weight {
-#       percentage = 100
-#     }
-#   }
-
+  
   template {
     min_replicas = 1
     max_replicas = 1
