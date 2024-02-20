32381aeca65d750126103e46544b9feacf1cc55f
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2024-02-20 00:57:48 +0000

    enables admin user for acr ( :( )

--- global/container-registry/main.tf
+++ global/container-registry/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.container-registry-resource-group.name
   location            = azurerm_resource_group.container-registry-resource-group.location
   sku                 = "Basic"
-  admin_enabled       = false
+  admin_enabled       = true
 
   identity {
     type = "SystemAssigned"
